08fee5d487b16ae94cff41ff609957ab2dcc50f6
supratim50 <supratimmondal50@gmail.com>
2021-06-17 16:43:04 +0530

    Add animation

--- src/components/buttons/Button.js
+++ src/components/buttons/Button.js
@@ -3,8 +3,10 @@ import React from "react";
 //CSS
 import "./Button.css";
 
-const Button = ({ text }) => {
-  return <div className="button">{text && text}</div>;
+const Button = ({ text, active }) => {
+  return (
+    <div className={`button ${active ? "active" : ""}`}>{text && text}</div>
+  );
 };
 
 export default Button;
--- src/components/cards/foodCards/FoodCard.js
+++ src/components/cards/foodCards/FoodCard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+
 import image from "../../../assets/images/pizza.png";
 import background from "../../../assets/images/background.svg";
 import Button from "../../buttons/Button";
@@ -7,25 +8,43 @@ import Button from "../../buttons/Button";
 import "./FoodCard.css";
 
 const FoodCard = () => {
+  const [active, setActive] = useState(false);
+
+  const setActiveFunc = () => {
+    setActive(!active);
+  };
+
   return (
-    <div className="card-box">
+    <div className={`card-box ${active ? "card-box-active" : ""}`}>
       {/* BACKGROUND SVG  */}
       <div className="background-box">
         <img className="background-image" src={background} />
       </div>
       {/* CARD IMAGE */}
-      <div className="image-box">
+      <div
+        className={`image-box ${active ? "image-box-active" : ""}`}
+        onClick={setActiveFunc}
+      >
         <img className="image" src={image} />
       </div>
       {/* CARD DETAILS */}
-      <div className="details-box">
-        <h4 className="heading">
+      <div className={`details-box ${active ? "details-box-active" : ""}`}>
+        <h4 className={`heading ${active ? "heading-active" : ""}`}>
           Double Cheese Margherita. loaded with extra cheese... oodies of it!{" "}
         </h4>
+        <p className={`food-details ${active ? "food-details-active" : ""}`}>
+          The term pizza was first recorded in the 10th century in a Latin
+          manuscript from the Southern Italian town of Gaeta in Lazio, on the
+          border with Campania.
+        </p>
 
         <div className="price-details">
-          <h1 className="price">$11.99</h1>
-          <Button text="Buy Now" />
+          <h1 className={`price ${active ? "price-active" : ""}`}>$11.99</h1>
+          {active ? (
+            <Button text="Buy Now" active />
+          ) : (
+            <Button text="Buy Now" />
+          )}
         </div>
       </div>
     </div>
